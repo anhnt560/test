03049e724722f88ce711bab87d9131a3b9a7f3cb
Tuan Anh Nguyen <anhnt_560@vnu.edu.vn>
2014-04-19 17:04:21 +0700

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -34,7 +34,7 @@ public class HelloWorld extends Activity {
     {
        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }  
-    //hehe
+    //haha
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
